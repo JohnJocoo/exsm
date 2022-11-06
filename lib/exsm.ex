68659a1c7d02b9a6e081e56374fed4964f2eba02
Anton Frolov <johnjocoo@gmail.com>
2022-11-06 22:02:03 +0000

    Added an example of State Machine syntax in test/player_test.exs
    Added macros stabs in lib/exsm.ex

--- lib/exsm.ex
+++ lib/exsm.ex
@@ -3,6 +3,94 @@ defmodule EXSM do
   Documentation for `EXSM`.
   """
 
+  defmacro __using__(opts) do
+    quote do
+      import unquote(__MODULE__)
+    end
+  end
+
+  ### State definition macros
+
+  defmacro state(name, do: block) do
+    IO.inspect({:state, name, block})
+    quote do
+
+    end
+  end
+
+  defmacro state(name) do
+    IO.inspect({:state, name})
+    quote do
+
+    end
+  end
+
+  defmacro describe(info) do
+    IO.inspect({:describe, info})
+    quote do
+
+    end
+  end
+
+  defmacro on_enter(do: block) do
+    IO.inspect({:on_enter, block})
+    quote do
+
+    end
+  end
+
+  defmacro on_enter(function) do
+    IO.inspect({:on_enter, function})
+    quote do
+
+    end
+  end
+
+  defmacro on_leave(do: block) do
+    IO.inspect({:on_leave, block})
+    quote do
+
+    end
+  end
+
+  defmacro on_leave(function) do
+    IO.inspect({:on_leave, function})
+    quote do
+
+    end
+  end
+
+  ### Transition table definition macros
+
+  defmacro transitions(do: block) do
+    IO.inspect({:transitions, block})
+    quote do
+
+    end
+  end
+
+  defmacro state_from <- expression do
+    IO.inspect({:from, state_from})
+    IO.inspect(expression)
+    quote do
+
+    end
+  end
+
+  defmacro action(do: expression) do
+    IO.inspect(expression)
+    quote do
+
+    end
+  end
+
+  defmacro action(function) do
+    IO.inspect(function)
+    quote do
+
+    end
+  end
+
   @doc """
   Hello world.
 
--- test/player_test.exs
+++ test/player_test.exs
@@ -1,7 +1,7 @@
 defmodule EXSM.PlayerTest do
   use ExUnit.Case
 
-  defmodule PlayerSM do
+  defmodule Player do
 
     def start_payback, do: :ok
     def pause_playback, do: :ok
@@ -9,40 +9,74 @@ defmodule EXSM.PlayerTest do
     def stop_playback, do: :ok
     def open_drawer, do: :ok
     def close_drawer, do: :ok
+    def start_detecting, do: :ok
+    def show_current_song, do: :ok
 
-    def store_cd_info({:cd_detected, good?, format}), do: :ok
-    def good_disk?({:cd_detected, good?, format}), do: good?
-    def auto_start?({:config, config}), do: Map.get(config, :auto_start, false)
+    def store_cd_info({:cd_detected, _disk, _format}), do: :ok
+    def good_disk?({:disk, readable?}), do: readable?
 
   end
 
   defmodule PlayerSM do
-    #use EXSM,
-    def fun() do
-    %{
-      states: [:stopped, :open, :empty, :playing, :paused],
-      events: [:play, :open_close, :stop, :cd_detected, :pause],
-      transitions: [
-        {:stopped, :play,        :playing, action: Player.start_payback/0},
-        {:stopped, :open_close,  :open,    action: Player.open_drawer/0},
-        {:stopped, :stop,        :stopped},
+    use EXSM
 
-        {:open,    :open_close,  :empty,   action: Player.close_drawer/0},
-
-        {:empty,   :cd_detected, :stopped, guard: Player.good_disk?/1,  action: Player.store_cd_info/1},
-        {:empty,   :cd_detected, :playing, guard: Player.auto_start?/1, action: Player.store_cd_info/1},
-
-        {:playing, :stop,        :stopped, action: Player.stop_playback/0},
-        {:playing, :pause,       :paused,  action: Player.pause_playback/0},
-        {:playing, :open_close,  :open,    action: [Player.stop_playback/0, Player.open_drawer/0]},
-
-        {:paused,  :pause,       :playing, action: Player.resume_playback/0},
-        {:paused,  :stop,        :stopped, action: Player.stop_playback/0},
-        {:paused,  :open_close,  :open,    action: [Player.stop_playback/0, Player.open_drawer/0]}
-      ]
-    }
+    state :stopped do
+      describe "Playback was stopped"
+      on_enter enter_stopped/2
+      on_leave leave_stopped/2
     end
 
+    state :empty do
+      @initial
+      describe "No cd, also initial state"
+      on_enter fn(state, _event) -> Player.start_detecting() end
+    end
+
+    state :playing do
+      describe "Playing a song now"
+      on_enter do: Player.show_current_song()
+    end
+
+    state :open do
+      describe "Drawer is open"
+    end
+
+    transitions do
+      :empty <- {:cd_detected, disk, _} = event >>> :stopped when Player.good_disk?(disk)
+        action do: Player.store_cd_info(event)
+
+      :stopped <- :play >>> :playing
+        action do: Player.start_payback()
+      :stopped <- :open_close >>> :open
+        action do: Player.open_drawer()
+      :stopped <- :stop >>> :stopped
+
+      :open <- :open_close >>> :empty
+        action Player.close_drawer/0
+
+      :playing <- :stop >>> :stopped
+        action do: Player.stop_playback()
+      :playing <- :pause >>> :paused
+        action do: Player.pause_playback()
+      :playing <- :open_close >>> :open
+        action do
+          Player.stop_playback()
+          Player.open_drawer()
+        end
+
+      :paused <- :pause >>> :playing
+        action do: Player.resume_playback()
+      :paused <- :stop >>> :stopped
+        action do: Player.stop_playback()
+      :paused <- :open_close >>> :open
+        action do
+          Player.stop_playback()
+          Player.open_drawer()
+        end
+    end
+
+    defp enter_stopped(state, _event), do: :ok
+    defp leave_stopped(state, _event), do: :ok
   end
 
   test "greets the world" do
