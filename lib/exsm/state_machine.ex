571b4f2b2eaf93feb3682f297067eb247c590ace
Anton Frolov <johnjocoo@gmail.com>
2022-12-14 23:45:17 +0000

    Added state id tracking to EXSM.StateMachine

--- lib/exsm/state_machine.ex
+++ lib/exsm/state_machine.ex
@@ -1,24 +1,29 @@
 defmodule EXSM.StateMachine do
   @moduledoc false
 
+  @type region :: atom()
+
   @type t :: %__MODULE__{
                module: module(),
-               current_states: %{atom() => EXSM.State.t()},
+               current_states: %{region() => EXSM.State.t()},
+               current_state_ids: %{region() => atom()},
                user_state: any()
              }
-  defstruct [:module, :current_states, :user_state]
+  defstruct [:module, :current_states, :current_state_ids, :user_state]
 
-  @spec new(module(), [EXSM.State.t()], Keyword.t()) :: __MODULE__.t()
+  @spec new(module(), [{atom(), EXSM.State.t()}], Keyword.t()) :: __MODULE__.t()
   def new(module, initial_states, opts) when is_atom(module) and length(initial_states) > 0 do
     repeated_regions =
-      Enum.frequencies_by(initial_states, &(&1.region))
+      Enum.frequencies_by(initial_states, fn {_, %EXSM.State{region: region}} -> region end)
       |> Enum.filter(fn {_, count} -> count > 1 end)
     case repeated_regions do
       [] ->
-        states_map = Map.new(initial_states, fn %EXSM.State{region: region} = state -> {region, state} end)
+        states_map = Map.new(initial_states, fn {_, %EXSM.State{region: region} = state} -> {region, state} end)
+        ids_map = Map.new(initial_states, fn {id, %EXSM.State{region: region}} -> {region, id} end)
         %__MODULE__{
           module: module,
           current_states: states_map,
+          current_state_ids: ids_map,
           user_state: Keyword.get(opts, :user_state)
         }
 
@@ -26,14 +31,13 @@ defmodule EXSM.StateMachine do
         {region, _} = List.first(repeated_regions)
         repeated_states =
           Enum.filter(initial_states, fn
-            %EXSM.State{region: ^region} -> true
-            %EXSM.State{} -> false
+            {_, %EXSM.State{region: ^region}} -> true
+            {_, %EXSM.State{}} -> false
           end)
-          |> Enum.map(&(&1.name))
+          |> Enum.map(fn {_, %EXSM.State{name: name}} -> name end)
         raise(ArgumentError,
           "initial states in the same region #{region} found #{inspect(repeated_states)} for module #{module}")
     end
-
   end
 
   @spec all_current_states(__MODULE__.t()) :: [EXSM.State.t()]
@@ -41,7 +45,7 @@ defmodule EXSM.StateMachine do
     Map.values(states)
   end
 
-  @spec current_state(__MODULE__.t(), atom() | nil) :: EXSM.State.t()
+  @spec current_state(__MODULE__.t(), region() | nil) :: EXSM.State.t()
   def current_state(state_machine, region \\ nil)
 
   def current_state(%__MODULE__{current_states: %{nil => state}}, nil) do
@@ -59,22 +63,40 @@ defmodule EXSM.StateMachine do
     end
   end
 
+  @spec current_state_id(__MODULE__.t(), region() | nil) :: atom()
+  def current_state_id(state_machine, region \\ nil)
+
+  def current_state_id(%__MODULE__{current_state_ids: %{nil => id}}, nil) do
+    id
+  end
+
+  def current_state_id(%__MODULE__{current_state_ids: ids}, region) do
+    Map.fetch!(ids, region)
+  end
+
   @spec user_state(__MODULE__.t()) :: any()
   def user_state(%__MODULE__{user_state: user_state}) do
     user_state
   end
 
-  @spec update_current_state(__MODULE__.t(), EXSM.State.t(), atom() | nil) :: __MODULE__.t()
+  @spec update_current_state(__MODULE__.t(), {atom(), EXSM.State.t()}, region() | nil) :: __MODULE__.t()
   def update_current_state(state_machine, current_state, region \\ nil)
 
   def update_current_state(%__MODULE__{current_states: %{nil => _state}} = state_machine,
-                           %EXSM.State{} = current_state,
+                           {current_state_id, %EXSM.State{} = current_state},
                            nil) do
-    %__MODULE__{state_machine | current_states: %{nil => current_state}}
+    %__MODULE__{
+      state_machine |
+      current_states: %{nil => current_state},
+      current_state_ids: %{nil => current_state_id}
+    }
   end
 
-  def update_current_state(%__MODULE__{current_states: states, module: module} = state_machine,
-                           %EXSM.State{region: region} = current_state,
+  def update_current_state(%__MODULE__{current_states: states,
+                                       current_state_ids: ids,
+                                       module: module
+                                      } = state_machine,
+                           {current_state_id, %EXSM.State{} = current_state},
                            region) do
     case Map.has_key?(states, region) do
       false ->
@@ -82,7 +104,11 @@ defmodule EXSM.StateMachine do
         raise ArgumentError, "region #{region_dbg} does not exist for state machine #{module}"
 
       true ->
-        %__MODULE__{state_machine | current_states: Map.replace!(states, region, current_state)}
+        %__MODULE__{
+          state_machine |
+          current_states: Map.replace!(states, region, current_state),
+          current_state_ids: Map.replace!(ids, region, current_state_id)
+        }
     end
   end
 
--- test/exsm_test.exs
+++ test/exsm_test.exs
@@ -2,6 +2,24 @@ defmodule EXSMTest do
   use ExUnit.Case
   doctest EXSM
 
+  defmodule StatesNone do
+    use EXSM
+  end
+
+  defmodule StatesOne do
+    use EXSM
+
+    state :one
+  end
+
+  defmodule StatesThree do
+    use EXSM
+
+    state :one
+    state :two
+    state :three
+  end
+
   test "defines __using__" do
     assert macro_exported?(EXSM, :__using__, 1)
   end
@@ -21,10 +39,12 @@ defmodule EXSMTest do
 
   test "defines on_enter" do
     assert macro_exported?(EXSM, :on_enter, 1)
+    assert macro_exported?(EXSM, :on_enter, 2)
   end
 
   test "defines on_leave" do
     assert macro_exported?(EXSM, :on_leave, 1)
+    assert macro_exported?(EXSM, :on_leave, 2)
   end
 
   test "defines transitions" do
@@ -37,5 +57,27 @@ defmodule EXSMTest do
 
   test "defines action" do
     assert macro_exported?(EXSM, :action, 1)
+    assert macro_exported?(EXSM, :action, 2)
+  end
+
+  test "states EXSMTest" do
+    assert [] == EXSM.states(EXSMTest)
+  end
+
+  test "states StatesNone" do
+    assert [] == EXSM.states(StatesNone)
+  end
+
+  test "states StatesOne" do
+    assert [%EXSM.State{name: :one}] == EXSM.states(StatesOne)
+  end
+
+  test "states StatesThree" do
+    assert 3 == length(EXSM.states(StatesThree))
+    assert MapSet.new([
+             %EXSM.State{name: :one},
+             %EXSM.State{name: :two},
+             %EXSM.State{name: :three}
+           ]) == MapSet.new(EXSM.states(StatesThree))
   end
 end
